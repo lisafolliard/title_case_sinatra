ec3c7ae088250cf58b3bdd1c459ce626119684b9
Austin Kincaid & Michael Summers <student@epicodus.com>
2015-08-03 10:19:34 -0700

    Added exception to our expection list to allow the first word to be capitalized

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -11,6 +11,10 @@ describe('String#title') do
   end
 
   it("does not capitalize specific words i.e. for, nor, an, etc.") do
-    expect(("nor the and but an a or for").titlecase()).to(eq("nor the and but an a or for"))
+    expect(("nor the and but an a or for").title_case()).to(eq("nor the and but an a or for"))
+  end
+
+  it("capitalizes the exceptions words if they are the first in the string") do
+    expect(("nor the and but an a or for").title_case()).to(eq("Nor the and but an a or for"))
   end
 end

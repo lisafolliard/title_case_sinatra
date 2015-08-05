23d98c13e31a6bbd341dd027cef9d9b9181a036a
Austin Kincaid & Lisa Folliard <student@epicodus.com>
2015-08-05 09:43:08 -0700

    add capybara spec

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -2,7 +2,7 @@ require('rspec')
 require('title_case')
 require('pry')
 
-describe('String#title') do
+describe('String#title_case') do
   it("capitalizes the first letter of a word") do
     expect(("onion").title_case()).to(eq("Onion"))
   end
